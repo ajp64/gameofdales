b5684c93cf4d5fd207ed96410348e018847a7dba
ajp64 <antonyjparnell@gmail.com>
2022-03-11 21:54:32 +0000

    added default time

--- src/ChooseTime/index.js
+++ src/ChooseTime/index.js
@@ -4,17 +4,35 @@ import { DateTime } from "luxon";
 import { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import css from "./index.module.css";
+import moment from "moment";
 
 export function ChooseTime() {
   const [copied, setCopied] = useState(false);
   const [day, setDay] = useState(null);
   const [hour, setHour] = useState(null);
   const [min, setMin] = useState(null);
+  const [defday, setDefDay] = useState("");
+  const [defmon, setDefMon] = useState("");
 
   const dt = DateTime.fromObject(
     { day: day, hour: hour, minute: min },
     { zone: "Pacific/Auckland" }
   );
+
+  useEffect(() => {
+    if (dt.day.toString().length === 1) {
+      setDefDay("0" + dt.day.toString());
+    } else {
+      setDefDay(dt.day.toString());
+    }
+    console.log(defday);
+    if (dt.month.toString().length === 1) {
+      setDefMon("0" + dt.month.toString());
+    } else {
+      setDefMon(dt.mon.toString());
+    }
+    console.log(defmon);
+  });
 
   const ukDt = dt.setZone("Europe/London");
 
@@ -57,34 +75,41 @@ export function ChooseTime() {
     copyChange();
   }, [copyChange]);
 
-  return (
-    <Space className={css.date} direction="vertical" size={16}>
-      <DatePicker
-        showTime
-        onChange={onChange}
-        onOk={onOk}
-        placeholder={"Choose Dale Time"}
-        className={css.datePicker}
-      />
-      <section className="section">
-        {copied ? (
-          <span style={{ color: "red" }}>Copied!</span>
-        ) : (
-          <span style={{ color: "red" }}>
-            Click times to copy to clipboard:
-          </span>
-        )}
-      </section>
-      <CopyToClipboard onCopy={onCopy} text={copyValue}>
-        <h2
-          className={css.date}
-          onClick={copyChange}
-        >{`Game Time in NZ: ${dt.toLocaleString(
-          DateTime.DATETIME_MED
-        )},   Game Time in UK: ${ukDt.toLocaleString(
-          DateTime.DATETIME_MED
-        )}`}</h2>
-      </CopyToClipboard>
-    </Space>
-  );
+  console.log(`${dt.year}-${defmon}-${defday}T09:00:00.886Z`, defday, defmon);
+
+  if (defmon) {
+    return (
+      <Space className={css.date} direction="vertical" size={16}>
+        <DatePicker
+          showTime
+          onChange={onChange}
+          onOk={onOk}
+          placeholder={"Choose Dale Time"}
+          className={css.datePicker}
+          defaultValue={moment(`${dt.year}-${defmon}-${defday}T09:00:00.886Z`)}
+        />
+        <section className="section">
+          {copied ? (
+            <span style={{ color: "red" }}>Copied!</span>
+          ) : (
+            <span style={{ color: "red" }}>
+              Click times to copy to clipboard:
+            </span>
+          )}
+        </section>
+        <CopyToClipboard onCopy={onCopy} text={copyValue}>
+          <h2
+            className={css.date}
+            onClick={copyChange}
+          >{`Game Time in NZ: ${dt.toLocaleString(
+            DateTime.DATETIME_MED
+          )},   Game Time in UK: ${ukDt.toLocaleString(
+            DateTime.DATETIME_MED
+          )}`}</h2>
+        </CopyToClipboard>
+      </Space>
+    );
+  } else {
+    return <></>;
+  }
 }
